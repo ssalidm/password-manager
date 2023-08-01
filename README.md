708dea5b3677eef44f36c32b1a11caa0a57cfa59
David Ssali <ssalidm@gmail.com>
2023-08-01 22:59:29 +0200

    Add final project v1.0

---
+++ README.md
@@ -0,0 +1,63 @@
+# Password Manager - Python GUI Application Version 1.0
+
+This is a simple password manager application built using Python and the Tkinter library. It allows users to generate secure passwords and store their website login details in a text file.
+
+## Features
+
+- Generate strong passwords consisting of letters (both upper and lower case), numbers, and symbols.
+- Store website login details (website URL, email/username, and password) in a local text file.
+- User-friendly graphical user interface (GUI) built with Tkinter.
+
+## Screenshots
+
+![Alt text](screenshots/image.png)
+
+## Getting Started
+
+Follow the steps below to run the Password Manager application on your local machine:
+
+1. Clone the repository:
+
+```bash
+git clone https://github.com/ssalidm/password-manager.git
+```
+
+2. Navigate to the project directory:
+
+```bash
+cd password-manager
+```
+
+3. Install the required packages (make sure you have Python installed):
+
+```bash
+pip install -r requirements.txt
+```
+
+4. Run the application:
+
+```bash
+python main.py
+```
+
+## How to Use
+
+1. Launch the Password Manager application using the steps provided above.
+
+2. **Generate Password:** Click on the "Generate Password" button to create a secure password. The generated password will be automatically filled in the password input field.
+
+3. **Save Website Details:** Enter the website URL, email/username, and password in the corresponding input fields. Click on the "Add" button to save the website details in a local text file named "data.txt."
+
+4. **Exit:** Close the application by clicking the "x" button on the window's title bar.
+
+## Contributing
+
+Contributions are welcome! If you find any bugs or want to add new features, feel free to create an issue or submit a pull request.
+
+## License
+
+This project is licensed under the [MIT License](https://github.com/ssalidm/password-manager/blob/main/LICENSE).
+
+## Acknowledgments
+
+- This project was inspired by the [App Brewery's](https://www.appbrewery.co/) Python course on Udemy.
---
+++ main.py
@@ -0,0 +1,91 @@
+#!/usr/bin/env python3
+import tkinter as tk
+from tkinter import messagebox
+from random import randint, choice, shuffle
+import pyperclip
+
+
+# ---------------------------- PASSWORD GENERATOR ------------------------------- #
+def generate_password():
+    letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u',
+               'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P',
+               'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+    numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+    symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+    password_letters = [choice(letters) for _ in range(randint(8, 10))]
+    password_numbers = [choice(numbers) for _ in range(randint(2, 4))]
+    password_symbols = [choice(symbols) for _ in range(randint(2, 4))]
+
+    password_list = password_letters + password_numbers + password_symbols
+    shuffle(password_list)
+
+    password = "".join(password_list)
+    entry_password.delete(0, tk.END)
+    entry_password.insert(0, password)
+    pyperclip.copy(password)
+
+
+# ---------------------------- SAVE PASSWORD ------------------------------- #
+def save():
+    website = entry_website.get()
+    email = entry_email.get()
+    password = entry_password.get()
+
+    if len(website) == 0 or len(password) == 0 or len(email) == 0:
+        messagebox.showinfo(title="Oops", message="Please don't leave any fields empty!")
+        return
+
+    is_ok = messagebox.askokcancel(title=website,
+                                   message=f"These are the details entered: \nEmail: {email} \nPassword: {password}"
+                                           f" \n\nIs it ok to save?")
+
+    if is_ok:
+        with open("data.txt", "a") as data_file:
+            data_file.write(f"{website} | {email} | {password}\n")
+            entry_website.delete(0, tk.END)
+            entry_password.delete(0, tk.END)
+
+
+# ---------------------------- UI SETUP ------------------------------- #
+root = tk.Tk()
+root.title("Password Manager")
+root.config(padx=40, pady=30)
+
+# create canvas
+canvas = tk.Canvas(width=200, height=200, highlightthickness=0)
+logo_img = tk.PhotoImage(file="logo.png")
+canvas.create_image(100, 100, image=logo_img)
+canvas.grid(column=1, row=0)
+
+# labels
+label_website = tk.Label(text="Website:")
+label_website.grid(column=0, row=1, sticky='e')
+
+label_email = tk.Label(text="Email/Username:")
+label_email.grid(column=0, row=2, sticky='e')
+
+label_password = tk.Label(root, text="Password:")
+label_password.grid(column=0, row=3, sticky='e')
+
+# Entries
+entry_website = tk.Entry(root, width=50)
+entry_website.focus()
+entry_website.grid(column=1, row=1, columnspan=2, pady=2)
+
+entry_email = tk.Entry(root, width=50)
+entry_email.insert(0, "name@example.com")
+entry_email.grid(column=1, row=2, columnspan=2, pady=2)
+
+entry_password = tk.Entry(root, width=30, bd=1)
+entry_password.grid(column=1, row=3, pady=3, padx=0, sticky='w')
+
+# Buttons
+button_generate_password = tk.Button(root, text="Generate Password", borderwidth=1, command=generate_password)
+button_generate_password.grid(column=2, row=3, padx=0, pady=3, sticky='e')
+
+button_Add = tk.Button(root, text="Add", width=43, borderwidth=1, command=save)
+button_Add.grid(column=1, row=4, columnspan=2, pady=3, padx=0, sticky='w')
+
+# run app
+root.mainloop()
